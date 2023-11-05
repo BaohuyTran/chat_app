d504e3c03b0500b1037d92826259385dec023c54
BaohuyTran <baohuyt11@gmail.com>
2023-11-05 21:49:53 +0700

    create controller route model, nodemon npm start

--- chat_node_server/index.js
+++ chat_node_server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const userRoute = require('./routes/userRoute')
+
+require('dotenv').config();
 
 const app = express();
-require('dotenv').config();
+const port = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI;
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/v1/users", userRoute);
 
-const port = process.env.PORT || 3000;
-const uri = process.env.ATLAS_URI;
+app.get('/', (req, res) => {
+  res.send('Welcome to the chat application that is not named yet...');
+})
 
 app.listen(port, (req, res) => {
   console.log(`Server is running on port: ${port}`);
