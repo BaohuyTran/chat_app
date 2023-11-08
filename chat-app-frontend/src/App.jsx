d9245c6eeee2d3a64228bceebe43ee5583b473da
BaohuyTran <baohuyt11@gmail.com>
2023-11-08 21:14:12 +0700

    Login signup component

--- chat-app-frontend/src/App.jsx
+++ chat-app-frontend/src/App.jsx
@@ -2,17 +2,23 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/chat";
 import Register from "./pages/resiter";
 import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Routes>
+      <NavBar />
+      <Container className="text-secondary">
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
   );
 }
--- chat-app-frontend/src/pages/Resiter.jsx
+++ chat-app-frontend/src/pages/Resiter.jsx
@@ -1,5 +1,32 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-  return (<>Register</>);
+  return (
+    <>
+      <Form>
+        <Row style={{
+          height: "100vh",
+          justifyContent: "center",
+          paddingTop: "10px",
+        }}>
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Sign up a new account</h2>
+              <Form.Control type="text" placeholder="Name" />
+              <Form.Control type="text" placeholder="Email" />
+              <Form.Control type="text" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Sign up
+              </Button>
+              <Alert variant="danger">
+                <p>An error has occurred!</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
 
 export default Register;
